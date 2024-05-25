bd05951ee742605765cd9769277da7a76123face
namnguy1 <namepchai1221@gmail.com>
2024-05-25 10:24:47 +0700

    ui first run

---
+++ script.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    // slidesPerGroup: 3,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1,
+        },
+        520:{
+            slidesPerView: 2,
+        },
+        950:{
+            slidesPerView: 3,
+        }
+    }
+  
+});
